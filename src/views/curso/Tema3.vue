c72326b5d69d04dacb6e5abf381b78ed23e8aee0
Francisco Lizcano <flizcanor@icloud.com>
2025-05-18 10:57:10 -0500

    Update Tema3.vue

--- src/views/curso/Tema3.vue
+++ src/views/curso/Tema3.vue
@@ -115,7 +115,7 @@
       .p-4.p-md-5
         figure
           .video
-            iframe(width="560" height="315" src="https://www.youtube.com/embed/9cqLwqWDCzs?si=F1b2M6SlLRnGqFf9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
+            iframe(width="560" height="315" src="https://www.youtube.com/embed/RxtT1okM6CM?si=Ge4r20Gpu4EaMeO9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
 
     #t_3_2.titulo-segundo(data-aos="flip-up")
       h2 #[span 3.2] La expresión dramática en la educación inicial
